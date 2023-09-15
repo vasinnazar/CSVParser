c2040bd5402f04ef8005b0a62643edf5d2d31fc9
nazar <n.vasin@lantan.info>
2023-09-15 13:08:28 +0700

    trash deleted

--- app/Action/CSV/StoreAction.php
+++ app/Action/CSV/StoreAction.php
@@ -14,23 +14,5 @@ class StoreAction
          $parser = new CSVParser($file);
          $products = $parser->parseCSV();
          $productRepository->setProducts($products);
-//         foreach ($products as $product) {
-//             Product::create([
-//                 'code' => $product[0],
-//                 'naming' => $product[1],
-//                 'level1' => $product[2],
-//                 'level2' => $product[3],
-//                 'level3' => $product[4],
-//                 'price' => $product[5],
-//                 'sp_price' => $product[6],
-//                 'quantity' => $product[7],
-//                 'property_fields' => $product[8],
-//                 'joint_purchases' => $product[9],
-//                 'measure_unit' => $product[10],
-//                 'image' => $product[11],
-//                 'main_page_display' => $product[12],
-//                 'description' => $product[13]
-//             ]);
-//         }
      }
 }
--- app/Helpers/CSVParser.php
+++ app/Helpers/CSVParser.php
@@ -16,22 +16,6 @@ class CSVParser
          $fileContent = file_get_contents($this->csv);
          $parsedContent = explode(';', $fileContent);
          return $this->structureContentToArray($parsedContent);
-//         $product = [];
-//         $products = [];
-//         foreach ( $parsedContent as $item ) {
-//             if(str_contains($item, ',,')) {
-//             $itemArray = explode(',,', $item);
-//             $lastIndex = count($itemArray);
-//                 array_push($product, $itemArray[0]);
-//                 array_push($products, $product);
-//                 $product = [];
-//                 array_push($product, str_replace(',', '', $itemArray[--$lastIndex]));
-//                 continue;
-//             }
-//             array_push($product, $item);
-//         }
-//          array_shift($products);
-//          return $products;
      }
     // structure data to nested array
      private function structureContentToArray($parsedContent)
